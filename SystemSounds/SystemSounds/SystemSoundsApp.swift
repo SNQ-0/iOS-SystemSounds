fe3c58d00990c22fcbc3f19d7082d3525f0881fb
SNQ <84154073+SNQ-0@users.noreply.github.com>
2021-12-21 20:06:33 +0900

    Add files via upload

---
+++ SystemSounds/SystemSounds/SystemSoundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemSoundsApp.swift
+//  SystemSounds
+//
+//  Created by 宮本大新 on 2021/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SystemSoundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
